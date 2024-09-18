34d08be2591d22b882d7f58745cf5e6e49cb67f3
MurugeshanRani <murugeshanrani@gmail.com>
2024-09-18 10:23:40 +0530

    first commit

---
+++ src/main/java/com/Pages/LoginTest.java
@@ -0,0 +1,9 @@
+package com.Pages;
+
+public class LoginTest {
+	private void loginPage() {
+		System.out.println("Welcome to Login Page");
+
+	}
+
+}
---
+++ src/test/java/com/GitHubDemo/GitHubDemo/loginTest.java
@@ -0,0 +1,8 @@
+package com.GitHubDemo.GitHubDemo;
+
+public class loginTest {
+	private void testPage() {
+		System.out.println("Welcome to Test Page");
+	}
+
+}
